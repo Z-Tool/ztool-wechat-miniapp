d0f1cf9e5f11c9c1970f641dbd87ffcbac40dfed
Jack <me@jarrekk.com>
2017-04-13 17:03:49 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,16 @@
+Page({
+    data: {
+        queries: [
+            {
+                url: "/pages/ip/ip",
+                title: "IP信息查询"
+            }, {
+                url: "/pages/domain/domain",
+                title: "域名信息查询"
+            }, {
+                url: "/pages/dns/dns",
+                title: "DNS记录查询"
+            }
+        ]
+    }
+});
---
+++ pages/profile/profile.js
@@ -0,0 +1,33 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello!',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  changeHello: function(e) {
+    var that = this
+    app.getUserInfo(function(userInfo){
+      that.setData({
+        motto: 'Hello ' + userInfo.nickName
+      })
+    })
+  },
+  onLoad: function () {
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
